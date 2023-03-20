dda27676dea545a670b0740f4f65e0fc76f2ddf0
AdonyasG <adonyas2000@gmail.com>
2023-03-20 17:02:01 -0400

    task fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
-export default function taskBlock (trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-    console.log(task);
-    console.log(task2);
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      const task = true;
+      const task2 = false;
+    }
+  
+    return [task, task2];
   }
-
-  return [task, task2];
-}
+  
