95ad1b527b3da15179b63fd6b83b7da6882d88cc
AdonyasG <adonyas2000@gmail.com>
2023-03-22 09:09:34 -0400

    type error fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
 export default function getResponseFromAPI(success) {
-  if (success) {
-    return Promise.resolve({
-      status: 200,
-      Body: 'Success',
-    });
-  }
-
-  return Promise.reject(new Error('The fake API is not working currently'));
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
 }
