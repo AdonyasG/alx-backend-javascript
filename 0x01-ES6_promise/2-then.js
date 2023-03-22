6d4423b1a199129234f32d4670edfffe06e2199f
AdonyasG <adonyas2000@gmail.com>
2023-03-22 09:48:00 -0400

    handled with then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,11 +2,11 @@ export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     if (promise) {
       resolve({ status: 200, body: 'Success' });
-      handleResponseFromAPI(promise).then(() => {
-        console.log('Got a response from the API');
-      });
     } else {
       reject(Error());
     }
+    handleResponseFromAPI(promise).then(() => {
+      console.log('Got a response from the API');
+    });
   });
 }
