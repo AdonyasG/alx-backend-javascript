1790511c4de4cea82a4335da3d23cc53e4afc4e7
AdonyasG <adonyas2000@gmail.com>
2023-03-22 16:46:31 -0400

    map fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName),
-    uploadPhoto(fileName)];
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
   return Promise.allSettled(promises)
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason.error,
-    })));
+    .then((results) => {
+      return results.map((result) => {
+        return {
+          status: result.status,
+          value: result.status === 'fulfilled' ? result.value : result.reason.message
+        };
+      });
+    });
 }
