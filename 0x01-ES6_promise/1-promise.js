0483f5b1d11234ece4b4de552a58b2cae084dabd
AdonyasG <adonyas2000@gmail.com>
2023-03-22 08:48:08 -0400

    error handle fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,10 @@
 export default function getResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, Body: 'Success' });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+  if (success) {
+    return Promise.resolve({
+      status: 200,
+      Body: 'Success',
+    });
+  }
+
+  return Promise.reject(new Error('The fake API is not working currently'));
 }
