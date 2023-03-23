76811b109ea49542cf3ec64a5b002fbe08417451
AdonyasG <adonyas2000@gmail.com>
2023-03-23 14:19:41 -0400

    alternate way added

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,4 +3,16 @@ import ClassRoom from './0-classroom';
 export default function initializeRooms() {
   const sizes = [19, 20, 34];
   return sizes.map((size) => new ClassRoom(size));
+
+  // alternate way
+  /**
+    const size = [19, 20, 34];
+  const arrays = [];
+
+  size.forEach((element) => {
+    arrays.push(new ClassRoom(element));
+  });
+
+  return arrays;
+  */
 }
