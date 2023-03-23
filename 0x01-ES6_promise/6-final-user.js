d79aae648a83a31a9f5c40cc03bd567b4c84669c
AdonyasG <adonyas2000@gmail.com>
2023-03-22 21:48:02 -0400

    final tiral

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,16 +3,18 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
+    .then((values) => {
       const res = [];
-      for (const result of results) {
-        if (result.status === 'fulfilled') {
-          res.push({ result });
+      values.forEach((value) => {
+        if (value.status === 'fulfilled') {
+          res.push(value);
         } else {
-          res.push({ status: result.status, value: `Error: ${result.reason.message}` });
+          res.push({
+            status: value.status,
+            value: `Error: ${value.reason.message}`,
+          });
         }
-      }
-
+      });
       return res;
     });
 }
