2bd793d202e88e09b9146dc551c28cc57eac2d5e
AdonyasG <adonyas2000@gmail.com>
2023-03-27 18:16:41 -0400

    updated

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,3 @@
-import getListStudentIds from './1-get_list_student_ids';
-import getListStudents from './0-get_list_students';
-
-export default function getStudentIdsSum() {
-  const array = getListStudentIds(getListStudents());
-  const sum = array.reduce((accumulator, number) => accumulator + number);
-  return sum;
+export default function getStudentIdsSum(students) {
+  return students.reduce((accumulator, student) => accumulator + student.id, 0);
 }
