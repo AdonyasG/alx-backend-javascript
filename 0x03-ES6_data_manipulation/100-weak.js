5949ab97ceabef6c4c1d660584d5c3b0b3012fc0
AdonyasG <adonyas2000@gmail.com>
2023-03-28 12:40:32 -0400

    fix 2

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,14 +3,14 @@ const weakMap = new WeakMap();
 export { weakMap };
 
 export function queryAPI(endpoint) {
-  const { protocol, name } = endpoint;
-  const key = `${protocol}_${name}`;
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, count);
 
-  const count = weakMap.get(key) || 0;
-  if (count >= 5) {
-    throw new Error('Endpoint load is high.');
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
   }
-  weakMap.set(key, count + 1);
-
-  return weakMap.get(key);
 }
