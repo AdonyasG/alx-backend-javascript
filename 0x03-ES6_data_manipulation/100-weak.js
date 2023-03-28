f60b78f543bf1352a3110538927c2ad779d95a5a
AdonyasG <adonyas2000@gmail.com>
2023-03-28 12:36:55 -0400

    fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,22 +1,14 @@
-// Create a new WeakMap instance
 const weakMap = new WeakMap();
 
-// Export the weakMap
-export { weakMap };
-
-// Define the queryAPI function
 export default function queryAPI(endpoint) {
-  // Check if the endpoint has been queried before
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
-  } else {
-    // Increment the number of times the endpoint has been queried
-    const count = weakMap.get(endpoint) + 1;
-    weakMap.set(endpoint, count);
+  const { protocol, name } = endpoint;
+  const key = `${protocol}_${name}`;
 
-    // Throw an error if the endpoint has been queried 5 or more times
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
+  const count = weakMap.get(key) || 0;
+  if (count >= 5) {
+    throw new Error('Endpoint load is high.');
   }
+  weakMap.set(key, count + 1);
+
+  return weakMap.get(key);
 }
