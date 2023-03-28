c1be6152cbb9cfcb3fd427f680c87ebd52bd6c09
AdonyasG <adonyas2000@gmail.com>
2023-03-28 10:01:51 -0400

    initialc ommit

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+  const cleanedValues = Array.from(set).filter((value) => value.startsWith(startString))
+    .map((value) => value.substring(startString.length));
+
+  return cleanedValues.join('-');
+}
