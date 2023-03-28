1e7972cbd7d2f26f2e031840a053499b6c00c71c
AdonyasG <adonyas2000@gmail.com>
2023-03-28 10:07:05 -0400

    exception added

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (startString === '' || startString === undefined || typeof startString !== 'string' || set.size === 0) {
     return '';
   }
   const cleanedValues = Array.from(set).filter((value) => value.startsWith(startString))
