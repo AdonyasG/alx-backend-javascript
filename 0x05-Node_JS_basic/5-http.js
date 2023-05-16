5b503c6c7e84471604f663ded20f5451679a599e
AdonyasG <adonyas2000@gmail.com>
2023-05-16 17:16:27 -0400

    fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,7 +3,6 @@ const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   }
