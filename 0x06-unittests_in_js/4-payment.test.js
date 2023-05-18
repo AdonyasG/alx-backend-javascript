deb7fa2b6cee8d5b932c3ced70408aece0849cca
AdonyasG <adonyas2000@gmail.com>
2023-05-18 16:45:05 -0400

    fix

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,7 +4,7 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('calculateNumber', () => {
+  it('call calculateNumber', () => {
     const stub = sinon.stub(Utils, 'calculateNumber');
     stub.returns(10);
     const spy = sinon.spy(console, 'log');
