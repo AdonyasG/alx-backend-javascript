187bb67eee1b37e87113f43e30c15f47d6458501
AdonyasG <adonyas2000@gmail.com>
2023-05-18 14:06:46 -0400

    intial commit

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const cal = require('./0-calcul');
+
+describe('cal', () => {
+  it('correct result', () => {
+    assert.strictEqual(cal(4, 1), 5);
+    assert.strictEqual(cal(4.7, 100), 105);
+    assert.strictEqual(cal(4.5, 1.2), 6);
+    assert.strictEqual(cal(4.1, 1.1), 5);
+  });
+
+  it('incorrect result', () => {
+    assert.notStrictEqual(cal(4, 1), 6);
+    assert.notStrictEqual(cal(4.7, 100), 100);
+    assert.notStrictEqual(cal(4.5, 1.2), 5);
+    assert.notStrictEqual(cal(4.1, 1.1), 6);
+  });
+});
