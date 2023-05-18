428b88994937e8a7bc4cf038257f9effcea50c68
AdonyasG <adonyas2000@gmail.com>
2023-05-18 17:35:25 -0400

    more test

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const getPaymentToken = require('./6-payment_token');
+
+describe('getPaymentToken', () => {
+  it('getPaymentToken', (done) => {
+    getPaymentToken(true)
+      .then((res) => {
+        expect(res).to.eql({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
