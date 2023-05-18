e4bea9876c82aa345f663a3b5dbce210a205a736
AdonyasG <adonyas2000@gmail.com>
2023-05-18 16:40:16 -0400

    iniital commit

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('calculateNumber', () => {
+    const stub = sinon.stub(Utils, 'calculateNumber');
+    stub.returns(10);
+    const spy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spy.calledOnceWithExactly('The total is: 10')).to.be.true;
+    stub.restore();
+    spy.restore();
+  });
+});
