dbb6138122e6de9e88d9e90c31aec7f85d5eb973
AdonyasG <adonyas2000@gmail.com>
2023-05-18 16:34:11 -0400

    using sinon

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,26 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    }
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+    }
+    if (typeof type !== 'string' || typeof a !== 'number' || typeof b !== 'number') {
+      throw TypeError('param 1 must be string and other numbers');
+    }
+    if (Number.isNaN(a) || Number.isNaN(b)) {
+      throw TypeError;
+    }
+    if (a === undefined || b === undefined) {
+      throw TypeError;
+    }
+  },
+};
+module.exports = Utils;
