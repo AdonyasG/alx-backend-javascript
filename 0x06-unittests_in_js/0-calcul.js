37c7ffb82fef194c1687d0ac1df0197f51129c8b
AdonyasG <adonyas2000@gmail.com>
2023-05-18 14:41:39 -0400

    more test cases

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,14 @@
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+const calculateNumber = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw TypeError('param must be both numbers');
+  }
+  if (Number.isNaN(a) || Number.isNaN(b)) {
+    throw TypeError;
+  }
+  if (a === undefined || b === undefined) {
+    throw TypeError;
+  }
+  return Math.round(a) + Math.round(b);
+};
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -15,4 +15,21 @@ describe('cal', () => {
     assert.notStrictEqual(cal(4.5, 1.2), 5);
     assert.notStrictEqual(cal(4.1, 1.1), 6);
   });
+
+  it('incorrect type', () => {
+    assert.throws(() => cal('b', 1), {
+      name: 'TypeError',
+      message: 'param must be both numbers',
+    });
+    assert.throws(() => cal(1, 'c'), {
+      name: 'TypeError',
+      message: 'param must be both numbers',
+    });
+  });
+
+  it('type error', () => {
+    assert.throws(() => cal(), {
+      name: 'TypeError',
+    });
+});
 });
