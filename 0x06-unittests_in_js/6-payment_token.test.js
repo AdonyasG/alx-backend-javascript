127b793be7326ddc08552967ffdeb514cc7d013a
AdonyasG <adonyas2000@gmail.com>
2023-05-19 17:20:21 -0400

    check

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,12 +5,9 @@ describe('getPaymentTokenFromAPI', () => {
   it('should return a resolved promise with successful response when success is true', () => {
     return getPaymentTokenFromAPI(true).then((result) => {
       expect(result).to.deep.equal({ data: 'Successful response from the API' });
-      throw new Error;
-    }).catch((error) => {
-      expect(error).to.equal(undefined);
     });
   });
-  
+
   it('should return a resolved promise with undefined when success is false', () => {
     return getPaymentTokenFromAPI(false).then((result) => {
       expect(result).to.be.undefined;
